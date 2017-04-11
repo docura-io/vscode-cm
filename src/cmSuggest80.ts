2832b9bcee410f5699bd5eebda3fded403a8ba4a
Tyler Lamb <lambwt@gmail.com>
2017-04-11 08:31:35 -0500

    Related to #39

--- src/cmSuggest80.ts
+++ src/cmSuggest80.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { CancellationToken, CompletionItem, CompletionItemProvider, Position, TextDocument } from 'vscode';
+import { CancellationToken, CompletionItem, CompletionItemProvider, CompletionList, Position, Range, TextDocument } from 'vscode';
 import { getCompiler } from './extension';
 import { CodeStatement } from './cmCompilerAdapter';
 
@@ -14,35 +14,40 @@ export class CM80CompletionItemProvider implements CompletionItemProvider {
         console.log(`Starting CM8.0 Suggestion Provider...`)
     }
 
-    public provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken): Thenable<CompletionItem[]> {
+    public provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken): Thenable<CompletionList> {
         return new Promise( (resolve, reject) => {
             
-            // this.compiler
-            this.compiler.runStatement( {
-                start: false,
-                code: `cvm_ac(\"${document.fileName.replace( /\\/g, '/' )}\", ${this.getOffset(document, position)});`,
-                successEx: /test/,
-                failureEx: /test/
-            } )
+            const prevChar = document.getText( new Range( position.translate(0, -1), position  ) );
+
+            document.save()
             .then( () => {
-                console.log('CM AC Success');
-                
-                fs.readFile( "c:/CetDev/version8.0/write/cm-ac-candidates.el", "utf-8", (err, data) => {
-                    var items: CompletionItem[] = [];
-                    let regex = new RegExp( `\\(cm-ac1\\s"(.[^"]*)"\\s"(.[^"]*)"\\s"(.[^"]*)"\\s\\(cons\\s\\(cm-ac-url\\s(\\d*)\\)\\s(\\d*)\\)\\)`, "g" );
-                    var match = null;
-                    while( match = regex.exec(data) ) {
-                        var item = new CompletionItem( `${match[1]}${match[2]}` );
-                        item.insertText = `${match[1]}`;
-                        items.push(item);
-                    }
+
+                this.compiler.runStatement( {
+                    start: false,
+                    code: `cvm_ac(\"${document.fileName.replace( /\\/g, '/' )}\", ${this.getOffset(document, position)});`,
+                    successEx: /test/,
+                    failureEx: /test/
+                } )
+                .then( () => {
+                    console.log('CM AC Success');
                     
-                    resolve(items);
-                } );
-            }, () => {
-                console.log('CM AC Failure');
-                reject();
-            })
+                    fs.readFile( "c:/CetDev/version8.0/write/cm-ac-candidates.el", "utf-8", (err, data) => {
+                        var items: CompletionItem[] = [];
+                        let regex = new RegExp( `\\(cm-ac1\\s"(.[^"]*)"\\s"(.[^"]*)"\\s"(.[^"]*)"\\s\\(cons\\s\\(cm-ac-url\\s(\\d*)\\)\\s(\\d*)\\)\\)`, "g" );
+                        var match = null;
+                        while( match = regex.exec(data) ) {
+                            var item = new CompletionItem( `${match[1]}${match[2]}` );
+                            item.insertText = `${match[1]}`;
+                            items.push(item);
+                        }
+                        
+                        resolve(new CompletionList( items, prevChar == "." ) );
+                    } );
+                }, () => {
+                    console.log('CM AC Failure');
+                    reject();
+                });
+            });
         })
     }
 
